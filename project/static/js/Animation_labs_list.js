55730d8abcb1701bac620626c580373925bd94b0
谢昌锟 <22920172204244@stu.xmu.edu.cn>
2019-11-12 16:27:59 +0800

    + 添加实验室页面添加实验室成员表格 + 实验室详情页面添加实验室成员名片 ~ 修改跳转为新件标签页 失 败

--- project/static/js/Animation_labs_list.js
+++ project/static/js/Animation_labs_list.js
@@ -2,25 +2,80 @@
 $(document).ready(function () {
     var lab_card = $(".lab_part");
 
-    function onSelect() {
-
-    }
+    layui.use('element', function () {
+        var element = layui.element;
+        var $ = layui.jquery;
+        var active = {
+            tabAdd: function () {
+                element.tabAdd('xbs_tab', {
+                    title: "实验室详情",
+                    content: "./lab_detail.html"
+                })
+            }
+        };
+        lab_card.on('click', function () {
+            var that = $(this), type = that.data(type);
+            active[type].call(this, that);
+        });
+    });
     lab_card.hover(
         function MouseIn() {
             $(this).animate({
-                    marginTop : -3,
-                    marginBottom : 20,
+                    // marginTop : -3,
+                    // marginBottom : 20,
+                    opacity: 0.8,
                 },
                 "fast"
             )
         },
         function MouseOut() {
             $(this).animate({
-                    marginTop : 5,
-                    marginBottom : 12,
+                    // marginTop : 5,
+                    // marginBottom : 12,
+                    opacity: 1,
                 },
                 "fast"
             )
         }
     )
-})
+});
+layui.use('element', function(){
+    var $ = layui.jquery
+        ,element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
+
+    //触发事件
+    var active = {
+        tabAdd: function(){
+            //新增一个Tab项
+            element.tabAdd('demo', {
+                title: '新选项'+ (Math.random()*1000|0) //用于演示
+                ,content: '内容'+ (Math.random()*1000|0)
+                ,id: new Date().getTime() //实际使用一般是规定好的id，这里以时间戳模拟下
+            })
+        }
+        ,tabDelete: function(othis){
+            //删除指定Tab项
+            element.tabDelete('demo', '44'); //删除：“商品管理”
+
+
+            othis.addClass('layui-btn-disabled');
+        }
+        ,tabChange: function(){
+            //切换到指定Tab项
+            element.tabChange('demo', '22'); //切换到：用户管理
+        }
+    };
+
+    $('.site-demo-active').on('click', function(){
+        var othis = $(this), type = othis.data('type');
+        active[type] ? active[type].call(this, othis) : '';
+    });
+
+//Hash地址的定位
+    var layid = location.hash.replace(/^#test=/, '');
+    element.tabChange('test', layid);
+
+    element.on('tab(test)', function(elem){
+        location.hash = 'test='+ $(this).attr('lay-id');
+    });
+});
